308f41ea304cdc61df5e16b278051505b586f035
Thiago Kuma <thiago.kuma@faculdadeimpacta.com.br>
2020-10-03 02:00:38 -0300

    Versao 3

--- prompt_setup.sh
+++ prompt_setup.sh
@@ -1,14 +1,8 @@
 apt update
-apt install -y wget ne git
+apt install -y ne
 
-wget https://gist.githubusercontent.com/insin/1425703/raw/f22c4231a7b28b8f420d79158b5229e5ebd3fcd9/bash_prompt.sh
+mv ~/.bashrc ~/.bashrc.bak
 
-chmod +x bash_prompt.sh
+cp /impacta/docker_linux/.bashrc.bak ~/.bashrc
 
-mv bash_prompt.sh ~/.bash_prompt.sh
-
-cp ~/.bashrc ~/.bashrc.bak
-
-echo '. ~/.bash_prompt.sh' > ~/.prompt
-
-cat ~/.bashrc.bak > cat ~/.prompt > ~/.bashrc
+echo 'EXECUTAR >>>> ". ~/.bashrc"'
